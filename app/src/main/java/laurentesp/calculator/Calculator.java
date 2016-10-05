d5b6d3cf98007521577f92b42bfa531ac8c28d2e
Laurent34 <laurent.esperance@gmail.com>
2016-10-05 09:49:30 +0200

    Add of the management of operands / add function / Unit Test for Add

---
+++ app/src/main/java/laurentesp/calculator/Calculator.java
@@ -0,0 +1,117 @@
+package laurentesp.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by SOEOSSA on 04/10/2016.
+ */
+
+public class Calculator {
+    //static List<StringBuffer> valOperand = new ArrayList<>();
+
+    static StringBuffer valOperand1 = new StringBuffer("0");
+    static StringBuffer valOperand2 = new StringBuffer("0");
+
+    // The attribute curOperand is used to determine during the calculation if the user is entering the first or the second operand
+    static int curOperand = 0;
+    static StringBuffer operator = new StringBuffer("");
+
+
+    static String getValOutToShowtoUser(String stringIn, String stringInTextView) {
+        StringBuffer stringBufOut = new StringBuffer("");
+        switch (stringIn) {
+            // The "C" button is used to clear the 2 operands and the operator
+            case "C":
+                valOperand1.setLength(0);
+                valOperand2.setLength(0);
+                curOperand = 0; // Go Back to first operand
+                stringBufOut.setLength(0);
+                stringBufOut.append("0");
+                break;
+
+            // In the case of the user enters the value 0 we have to check if the operand is not already null because it's useless to concatenate zeros
+            case "0":
+                if (stringInTextView.equals("0")) {
+                    stringBufOut.append(stringInTextView);
+                } else {
+                    stringBufOut.append(stringInTextView);
+                    stringBufOut.append(stringIn);
+                }
+
+                break;
+
+            case "+":
+                curOperand = 1;
+                stringBufOut.append("0");
+                operator.setLength(0);
+                operator.append("Add");
+                break;
+
+            case "=":
+                curOperand = 0;
+                stringBufOut.setLength(0);
+                stringBufOut.append(getResultFromOperatorOnOperands(valOperand1.toString(),valOperand2.toString(),operator.toString()));
+                valOperand1.setLength(0);
+                valOperand2.setLength(0);
+                break;
+
+            default:
+                // The default case is for 1..9 buttons / Only requirement, if a 0 has been entered previously we won't show it
+                if (stringInTextView.equals("0")) {
+                    stringBufOut.append(stringIn);
+                } else {
+                    stringBufOut.append(stringInTextView);
+                    stringBufOut.append(stringIn);
+                }
+
+                if (curOperand == 0) {
+                    valOperand1 = stringBufOut;
+                } else {
+                    valOperand2 = stringBufOut;
+                }
+        }
+
+        return stringBufOut.toString();
+    }
+
+    static String getLastOperand() {
+        StringBuffer stringBufOut = new StringBuffer("");
+        if (curOperand == 0) {
+            stringBufOut = valOperand1;
+        } else {
+            stringBufOut = valOperand2;
+        }
+
+        return stringBufOut.toString();
+    }
+
+    static String getResultFromOperatorOnOperands(String operand1, String operand2, String operator) {
+        double valOperand1 =  Double.valueOf(operand1);
+        double valOperand2 = Double.valueOf(operand2);
+        double valOut = 0;
+        switch (operator) {
+            case "Add":
+                valOut = valOperand1+valOperand2;
+                break;
+            default:
+                valOut = valOperand1;
+        }
+        return removeFractionalPartFromDoubleIfNotNecessary(valOut);
+    }
+
+    static String removeFractionalPartFromDoubleIfNotNecessary(double valIn){
+        double fractionalPart = valIn % 1;
+        double integralPart = valIn - fractionalPart;
+        int intPart = (int)integralPart;
+        String valOut;
+
+        if ((valIn - integralPart) != 0.0) {
+            valOut = new String(Double.toString(valIn));
+        } else {
+            valOut = new String(Integer.toString(intPart));
+        }
+        return valOut;
+    }
+
+}
--- app/src/test/java/laurentesp/calculator/ExampleUnitTest.java
+++ app/src/test/java/laurentesp/calculator/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package laurentesp.calculator;
 
 import org.junit.Test;
 
+import static laurentesp.calculator.Calculator.*;
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +14,25 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void shouldReturnTrueIfTheResultOfAddMethodIsOK() throws Exception {
+        assertEquals("3", getResultFromOperatorOnOperands("2","1","Add"));
+    }
+
+    @Test
+    public void shouldReturnFalseIfTheResultOfAddMethodIsOK() throws Exception {
+        assertNotEquals("5.0", getResultFromOperatorOnOperands("2","1","Add"));
+    }
+
+    @Test
+    public void shouldReturnTrueIfCanRemoveFractionnalPart() throws Exception {
+        assertEquals("0", removeFractionalPartFromDoubleIfNotNecessary(0.0));
+    }
+
+    @Test
+    public void shouldReturnTrueIfShouldNotRemoveFractionnalPart() throws Exception {
+        assertEquals("0.1", removeFractionalPartFromDoubleIfNotNecessary(0.1));
     }
 }
